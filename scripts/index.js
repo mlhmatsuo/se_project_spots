087e6d1e9a32db79f899a38e8a26c6b7cd416d2d
MarkMatsuo <marklhmatsuo@gmail.com>
2024-08-15 19:10:22 -0700

    add modal functionality

--- scripts/index.js
+++ scripts/index.js
@@ -24,3 +24,25 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
   },
 ];
+
+const profileEditButton = document.querySelector(".profile__edit-button");
+
+const editProfileModal = document.querySelector("#edit-profile-modal");
+
+const profileCloseButton = document.querySelector(".modal__close-button");
+
+function openModal() {
+  editProfileModal.classList.add("modal__opened");
+}
+
+function closeModal() {
+  editProfileModal.classList.remove("modal__opened");
+}
+
+profileEditButton.addEventListener("click", function () {
+  openModal();
+});
+
+profileCloseButton.addEventListener("click", function () {
+  closeModal();
+});
